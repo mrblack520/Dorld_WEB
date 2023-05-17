6cc8d75a2bbc62658e2f6eebbf341f390621d494
Mr_Black <70857551+mrblack520@users.noreply.github.com>
2023-05-17 19:09:04 +0500

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Models\User;
 use Auth;
 use DB;
+use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
     public function login(Request $request)
@@ -14,21 +15,24 @@ class UserController extends Controller
         $email       = $request->email ;
         $password    = $request->password ;
 
-        $check_login = DB::table("users")->where(['email'=>$email,'password'=>$password])->first();
+        $check_login = DB::table("users")->where(['email'=>$email])->first();
+        $pass = $check_login->password;
         // dd($check_login);
         //check if a user exists with the given crediantals
-
-        if($check_login == false)
-        {
-            // $userData = User::where('id' , $check_login[0]->id)->first();
-            return redirect('page_login')-> with('error' , 'No Data found Invalid Crediantals');
+        if(Hash::check($password, $pass)){
+            //enter your code
+        // }
+        // if($check_login == false)
+        // {
+        //     // $userData = User::where('id' , $check_login[0]->id)->first();
+            return redirect('dashboard')-> with('error' , 'No Data found Invalid Crediantals');
 
         }
         else
         {
             
             session()->put('userauth'); // stored data into userauth session
-            return redirect('dashboard');
+            return redirect('page_login');
         }
     }
 }
